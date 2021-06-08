a00b5f577ee241246833aaa9ba5e605921ebd4e3
dennis <dlum904@gmail.com>
2021-06-08 14:32:38 -0400

    added quantity selector

--- src/Components/Top_Section/top_section.js
+++ src/Components/Top_Section/top_section.js
@@ -6,31 +6,49 @@ import { PRODUCTS } from '../../App.js'
 export default function TopSection({id, cart, addToCart}) {
 
     // Hook for what color is selected
-    const [color, setColor] = useState(
+    const [form, setForm] = useState(
         {
             black: "black ✔",
-            beige: "beige"
+            beige: "beige",
+            quantity: 1
         }
     );
     // Changes selection to black
     const selectBlack = () => {
-        setColor(
+        setForm(
             {
                 black: "black ✔",
-                beige: "beige "
+                beige: "beige ",
+                quantity: form.quantity
             }
         )
     }
     // Changes selection to beige
     const selectBeige = () => {
-        setColor(
+        setForm(
             {
                 black: "black ",
-                beige: "beige ✔"
+                beige: "beige ✔",
+                quantity: form.quantity
             }
         )
     }
         
+    const adjustQuantity = (num) => {
+
+        const oldQuantity = form.quantity;
+        let newQuantity = oldQuantity + num;
+        if (newQuantity < 1) newQuantity = 1;
+        setForm(
+            {
+                black: form.black,
+                beige: form.beige,
+                quantity: newQuantity
+            }
+        )
+
+    }
+
     return (
         <div className="top-section">
             main product here
@@ -46,13 +64,22 @@ export default function TopSection({id, cart, addToCart}) {
                 {/* Right side with all the selectors */}
                 <div className="product-right">
                     <p>{PRODUCTS[id].price}</p>
-                    <div className="color-select">
-                        <div className="color">color</div>
-                        <div className="color-black" onClick={() => selectBlack()}>{color.black}</div>
-                        <div className="color-beige" onClick={() => selectBeige()}>{color.beige}</div>
-                    </div>
+                    <form>
 
-                    <button onClick={() => addToCart(id)}> Add to Cart</button>
+                        <div className="color-select">
+                            <div className="color">color</div>
+                            <div className="color-black" onClick={() => selectBlack()}>{form.black}</div>
+                            <div className="color-beige" onClick={() => selectBeige()}>{form.beige}</div>
+                        </div>
+                        <br/>
+                        <div className="quantity-select">
+                            <input type="button" className="minus" onClick={() => adjustQuantity(-1)} value="-"/>
+                            <div>quantity ( {form.quantity} )</div>
+                            <input type="button" className="plus" onClick={() => adjustQuantity(1)} value="+"/>
+                        </div>
+                        <br/>
+                        <input type="button" onClick={() => addToCart(id)} value="add to cart"/>
+                    </form>
                 </div>
 
             </div>
