62cdee5aa7d7973d129fc704a345a2094f46352f
dennis <dlum904@gmail.com>
2021-06-08 17:15:14 -0400

    added size selector w/o using select tag.. ty for that..

--- src/Components/Top_Section/top_section.js
+++ src/Components/Top_Section/top_section.js
@@ -10,16 +10,28 @@ export default function TopSection({id, cart, addToCart}) {
         {
             black: "black ✔",
             beige: "beige",
-            quantity: 1
+            quantity: 1,
+            size: "M"
         }
     );
+
+    // Hook to show the size dropdown: if set to false, it will not show, if true it will show it
+    const [show, setShow] = useState(false)
+
+    // Changes the show state to true, if it's false, else set to true
+    const toggleShow = () => {
+        console.log("toggled")
+        !show ? setShow(true) : setShow(false);
+    }
+
     // Changes selection to black
     const selectBlack = () => {
         setForm(
             {
                 black: "black ✔",
                 beige: "beige ",
-                quantity: form.quantity
+                quantity: form.quantity,
+                size: form.size
             }
         )
     }
@@ -29,13 +41,15 @@ export default function TopSection({id, cart, addToCart}) {
             {
                 black: "black ",
                 beige: "beige ✔",
-                quantity: form.quantity
+                quantity: form.quantity,
+                size: form.size
             }
         )
-    }
         
+    }
+     
+    // increase/decrease quantity
     const adjustQuantity = (num) => {
-
         const oldQuantity = form.quantity;
         let newQuantity = oldQuantity + num;
         if (newQuantity < 1) newQuantity = 1;
@@ -43,10 +57,25 @@ export default function TopSection({id, cart, addToCart}) {
             {
                 black: form.black,
                 beige: form.beige,
-                quantity: newQuantity
+                quantity: newQuantity,
+                size: form.size
             }
         )
 
+    }
+
+    // changes the size based on selection
+    const selectSize = (size) => {
+        console.log("size selected")
+        setForm(
+            {
+                black: form.black,
+                beige: form.beige,
+                quantity: form.quantity,
+                size: size
+            }
+        )
+        toggleShow()
     }
 
     return (
@@ -65,18 +94,42 @@ export default function TopSection({id, cart, addToCart}) {
                 <div className="product-right">
                     <p>{PRODUCTS[id].price}</p>
                     <form>
-
+                        {/* COLOR SELECT */}
                         <div className="color-select">
                             <div className="color">color</div>
                             <div className="color-black" onClick={() => selectBlack()}>{form.black}</div>
                             <div className="color-beige" onClick={() => selectBeige()}>{form.beige}</div>
                         </div>
                         <br/>
+                        {/* QUANTITY SELECT */}
                         <div className="quantity-select">
                             <input type="button" className="minus" onClick={() => adjustQuantity(-1)} value="-"/>
                             <div>quantity ( {form.quantity} )</div>
                             <input type="button" className="plus" onClick={() => adjustQuantity(1)} value="+"/>
                         </div>
+                        <br/>
+                        {/* SIZE SELECT */}
+                        <div className="size-btn" onClick={() => toggleShow()}>
+                            <div>size</div>
+                            <div> ▿ </div>
+                        </div>
+                        {show ? (
+                            <div className="sizeDropdown">
+                                <ul>
+                                    <li onClick={() => selectSize("XS")}>XS</li>
+                                    <li onClick={() => selectSize("S")}>S</li>
+                                    <li onClick={() => selectSize("M")}>M</li>
+                                    <li onClick={() => selectSize("L")}>L</li>
+                                    <li onClick={() => selectSize("XL")}>XL</li>
+                                    <li onClick={() => selectSize("XXL")}>XXL</li>
+                                </ul>
+                                <div className="dropdown-bg" onClick={()=> toggleShow()}></div>
+                            </div>
+                        ) :
+                        null }
+                        <br/>
+                        <a href="/">what's my size?</a>
+                        <br/>
                         <br/>
                         <input type="button" onClick={() => addToCart(id)} value="add to cart"/>
                     </form>
